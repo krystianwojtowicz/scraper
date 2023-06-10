5beae606431563d241a7c9e60f59431c1d53bb26
Krystian <krystian1wojtowicz@gmail.com>
2023-06-10 12:46:05 +0200

    iterating throug the paginator

--- index.js
+++ index.js
@@ -12,27 +12,45 @@ axios(url).then((res) => {
     const linkedHTML = res.data;
     const linked$ = cheerio.load(linkedHTML);
     const specificElement = linked$(
-      '.productname:contains("ACTIVE CLEAN 10L - Koncentrat aktywnego preparatu silnie myjącego")'
+      '.productname:contains("FLOOR HOME & OFFICE 10L - Perfumowany, skoncentrowany płyn do mycia podłóg")'
     );
-    // if (specificElement.length > 0) {
-    //   const parent = specificElement.parent().parent();
-    //   const elementWithPrice = parent.find(".price.f-row");
-    //   const emWithPrice = elementWithPrice.find("em");
-    //   const price = emWithPrice.text();
-    //   console.log(price);
-    // } else {
-    const ulElement = linked$("ul.paginator");
-    const paginator = ulElement.eq(1);
-    const children = paginator.find("li");
-    const childrenArray = children.toArray();
-    childrenArray.shift();
-    childrenArray.pop();
-    const modifiedChildren = linked$(childrenArray);
-    const oddElements = modifiedChildren.filter((index) => index % 2 === 0);
-    const trimmedElements = oddElements.slice(1);
-    trimmedElements.each((index, element) => {
-      console.log(linked$(element).text());
-    });
-    // }
+    if (specificElement.length > 0) {
+      const parent = specificElement.parent().parent();
+      const elementWithPrice = parent.find(".price.f-row");
+      const emWithPrice = elementWithPrice.find("em");
+      const price = emWithPrice.text();
+      console.log(price);
+    } else {
+      const ulElement = linked$("ul.paginator");
+      const paginator = ulElement.eq(1);
+      const children = paginator.find("li");
+      const childrenArray = children.toArray();
+      childrenArray.shift();
+      childrenArray.pop();
+      const modifiedChildren = linked$(childrenArray);
+      const oddElements = modifiedChildren.filter((index) => index % 2 === 0);
+      const trimmedElements = oddElements.slice(1);
+
+      let secondPageLink = trimmedElements.eq(0).find("a").attr("href");
+      secondPageLink = secondPageLink.substring(1);
+      secondPageLink = secondPageLink.slice(0, -1);
+      let index = 2;
+      console.log(secondPageLink);
+      const searching = (index) => {
+        axios(url + secondPageLink + index).then((res) => {
+          const secondPageHTML = res.data;
+          const secondPage$ = cheerio.load(secondPageHTML);
+          const specificElement = secondPage$(
+            '.productname:contains("FLOOR HOME & OFFICE 10L - Perfumowany, skoncentrowany płyn do mycia podłóg")'
+          );
+          if (specificElement.length > 0) console.log("product was found");
+          else {
+            index++;
+            searching(index);
+          }
+        });
+      };
+      searching(index);
+    }
   });
 });
