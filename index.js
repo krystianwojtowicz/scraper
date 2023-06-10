a321e4651576f9e5f2b292f9b3f1b507ed7a7631
Krystian <krystian1wojtowicz@gmail.com>
2023-06-10 16:31:30 +0200

    searching-on-every-page

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function searchSpecificElement(page, selector, keyword) {
 }
 
 axios(url).then((res) => {
-  const foundElements = [];
+  let productNames = [];
   const html = res.data;
   const $ = cheerio.load(html);
   const elementWithTitle = $('a[title="CHEMIA OBIEKTOWA"]');
@@ -35,56 +35,50 @@ axios(url).then((res) => {
       const price = emWithPrice.text();
       //   console.log(price);
       //   console.log(specificElement);
-      const productNames = specificElement
+      productNames = specificElement
         .map((_, element) => linked$(element).text())
         .get();
-      console.log("Products found:");
-      console.log(productNames);
-    } else {
-      const ulElement = linked$("ul.paginator");
-      const paginator = ulElement.eq(1);
-      const children = paginator.find("li");
-      const childrenArray = children.toArray();
-      childrenArray.shift();
-      childrenArray.pop();
-      const modifiedChildren = linked$(childrenArray);
-      const oddElements = modifiedChildren.filter((index) => index % 2 === 0);
-      const trimmedElements = oddElements.slice(1);
-
-      let secondPageLink = trimmedElements.eq(0).find("a").attr("href");
-      secondPageLink = secondPageLink.substring(1);
-      secondPageLink = secondPageLink.slice(0, -1);
-      let index = 2;
-
-      const searching = (index) => {
-        axios(url + secondPageLink + index).then((res) => {
-          const secondPageHTML = res.data;
-          const secondPage$ = cheerio.load(secondPageHTML);
-          const specificElements = searchSpecificElement(
-            secondPage$,
-            ".productname",
-            searchKeyword
-          );
-          if (specificElements.length > 0) {
-            specificElements.each((_, element) => {
-              foundElements.push(secondPage$(element));
-              //   foundElements.push(secondPage$(element).text());
-            });
-            console.log("Products found on page " + index);
-            console.log(foundElements);
-          } else {
-            if (index <= trimmedElements.length) {
-              index++;
-              searching(index);
-            } else {
-              console.log("No more products found");
-              console.log(foundElements);
-            }
-          }
-        });
-      };
-
-      searching(index);
     }
+    const ulElement = linked$("ul.paginator");
+    const paginator = ulElement.eq(1);
+    const children = paginator.find("li");
+    const childrenArray = children.toArray();
+    childrenArray.shift();
+    childrenArray.pop();
+    const modifiedChildren = linked$(childrenArray);
+    const oddElements = modifiedChildren.filter((index) => index % 2 === 0);
+    const trimmedElements = oddElements.slice(1);
+
+    let secondPageLink = trimmedElements.eq(0).find("a").attr("href");
+    secondPageLink = secondPageLink.substring(1);
+    secondPageLink = secondPageLink.slice(0, -1);
+    let index = 2;
+
+    const searching = (index) => {
+      axios(url + secondPageLink + index).then((res) => {
+        const secondPageHTML = res.data;
+        const secondPage$ = cheerio.load(secondPageHTML);
+        const specificElements = searchSpecificElement(
+          secondPage$,
+          ".productname",
+          searchKeyword
+        );
+        if (specificElements.length > 0) {
+          productNames.push(
+            ...specificElements.map((_, element) => secondPage$(element).text())
+          );
+        }
+        if (index <= trimmedElements.length) {
+          index++;
+          searching(index);
+        } else {
+          productNames
+            ? console.log(productNames)
+            : console.log("product not found");
+        }
+      });
+    };
+
+    searching(index);
   });
 });
